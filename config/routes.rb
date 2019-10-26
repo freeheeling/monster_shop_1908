f46c1e26ac769969883f471acce210c04302560c
Michael Cooper <macoop2363@gmail.com>
2019-10-26 17:30:14 -0600

    Add route for merchant to add and create items

--- config/routes.rb
+++ config/routes.rb
@@ -44,7 +44,8 @@ Rails.application.routes.draw do
   namespace :merchant do
     get '/', to: 'dashboard#index', as: 'dashboard'
     get '/items', to: 'items#index', as: 'user_items'
-    get '/orders/:order_id', to: 'orders#show'
+    get '/items/new', to: 'items#new'
     patch '/items/:item_id/:activate_deactivate', to: 'items#activate_deactivate'
+    get '/orders/:order_id', to: 'orders#show'
   end
 end
