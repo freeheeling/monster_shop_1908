67ea9f314d1cb59c63fcd19eabcf3b07236b3583
Michael Cooper <macoop2363@gmail.com>
2019-10-26 18:34:03 -0600

    Add test that forms must be filled out

--- spec/features/merchants/items/create_spec.rb
+++ spec/features/merchants/items/create_spec.rb
@@ -119,6 +119,38 @@ RSpec.describe 'As a merchant' do
         expect(page).to have_content('Price must be greater than 0')
         expect(page).to have_content('Inventory must be greater than 0')
       end
+
+      it 'forms cannot be left blank' do
+        meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80_203)
+        merchant_admin = meg.users.create!(
+          name: 'Bob',
+          address: '123 Main',
+          city: 'Denver',
+          state: 'CO',
+          zip: 80_233,
+          email: 'bob@email.com',
+          password: 'secure',
+          role: 2
+        )
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merchant_admin)
+
+        visit merchant_user_items_path
+
+        click_on 'Add New Item'
+
+        expect(current_path).to eq(merchant_items_new_path)
+
+        fill_in :name, with: nil
+        fill_in :price, with: 5
+        fill_in :description, with: nil
+        fill_in :image, with: 'https://images-na.ssl-images-amazon.com/images/I/51HMpDXItgL._SX569_.jpg'
+        fill_in :inventory, with: 5
+
+        click_button 'Create Item'
+
+        expect(page).to have_content('Name can\'t be blank')
+        expect(page).to have_content('Description can\'t be blank')
+      end
     end
   end
 end
