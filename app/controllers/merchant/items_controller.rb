4a9d38dd3077eea7a4b0f36f4d046699e612e0b0
Michael Cooper <macoop2363@gmail.com>
2019-10-26 18:05:57 -0600

    Add functionality for merchant item creation

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -5,6 +5,20 @@ class Merchant::ItemsController < Merchant::BaseController
     @items = current_user.merchant.items
   end
 
+  def new; end
+
+  def create
+    merchant = current_user.merchant
+    item = merchant.items.create(item_params)
+    if item.save
+      flash[:success] = 'Item was successfully created!'
+      redirect_to merchant_user_items_path
+    else
+      flash[:error] = item.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
   def activate_deactivate
     if params[:activate_deactivate] == 'deactivate'
       item = Item.find(params[:item_id])
@@ -12,5 +26,11 @@ class Merchant::ItemsController < Merchant::BaseController
       flash[:notice] = "#{item.name} is now deactivated and is no longer for sale."
       redirect_to merchant_user_items_path
     end
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price, :inventory, :image)
   end
 end
