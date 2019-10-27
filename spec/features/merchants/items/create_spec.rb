fb7fb1925245fc21c57f0cb25ecf37cfcc5fa196
Michael Cooper <macoop2363@gmail.com>
2019-10-26 18:04:23 -0600

    Modify test to fix issues with original test and to more accurately reflect the user story

--- spec/features/merchants/items/create_spec.rb
+++ spec/features/merchants/items/create_spec.rb
@@ -17,33 +17,34 @@ RSpec.describe 'As a merchant' do
         )
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merchant_admin)
 
-        name = 'Chamois Buttr'
-        price = 18
-        description = "No more chaffin'!"
-        image_url = 'https://images-na.ssl-images-amazon.com/images/I/51HMpDXItgL._SX569_.jpg'
-        inventory = 25
-
-        visit merchant_items_new_path
+        visit merchant_user_items_path
 
         click_on 'Add New Item'
 
-        expect(page).to have_link(@brian.name)
-        expect(current_path).to eq("/merchants/#{@brian.id}/items/new")
-        fill_in :name, with: name
-        fill_in :price, with: price
-        fill_in :description, with: description
-        fill_in :image, with: image_url
-        fill_in :inventory, with: inventory
+        expect(current_path).to eq(merchant_items_new_path)
+
+        fill_in :name, with: 'Chamois Buttr'
+        fill_in :price, with: 18
+        fill_in :description, with: "No more chaffin'!"
+        fill_in :image, with: 'https://images-na.ssl-images-amazon.com/images/I/51HMpDXItgL._SX569_.jpg'
+        fill_in :inventory, with: 25
 
         click_button 'Create Item'
 
+        new_item = Item.last
+
+        expect(new_item.name).to eq('Chamois Buttr')
+        expect(new_item.price).to eq(18)
+        expect(new_item.description).to eq("No more chaffin'!")
+        expect(new_item.image).to eq('https://images-na.ssl-images-amazon.com/images/I/51HMpDXItgL._SX569_.jpg')
+        expect(new_item.inventory).to eq(25)
+        expect(new_item.active?).to eq(true)
+
+        expect(page).to have_content('Item was successfully created!')
+
         within "#item-#{Item.last.id}" do
-          expect(page).to have_content(name)
-          expect(page).to have_content("Price: $#{new_item.price}")
-          expect(page).to have_css("img[src*='#{new_item.image}']")
-          expect(page).to have_content('Active')
-          expect(page).to_not have_content(new_item.description)
-          expect(page).to have_content("Inventory: #{new_item.inventory}")
+          expect(page).to have_content('Chamois Buttr')
+          expect(page).to have_content('Deactivate Item')
         end
       end
     end
