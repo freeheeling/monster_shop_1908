f0974ce0668784479ce462a154df3e1954fe9d0c
Laura Schulz <lrs8810@gmail.com>
2019-10-27 21:27:21 -0600

    Add update_item action to Merchant::OrderController
    Co-Authored-By: Scott Regenthal <freeheeling@users.noreply.github.com>

--- app/controllers/merchant/orders_controller.rb
+++ app/controllers/merchant/orders_controller.rb
@@ -5,4 +5,16 @@ class Merchant::OrdersController < Merchant::BaseController
     @order = Order.find(params[:order_id])
     @item_orders = @order.find_order(current_user.merchant_id)
   end
+
+  def update_item
+    item_order = ItemOrder.find(params[:item_order_id])
+    item_order.update_attributes(status: 1)
+    item_order.update_item_inventory
+    flash[:success] = "The #{item_order.item.name} is now fulfilled."
+    if item_order.order.all_items_fulfilled?
+      item_order.order.update_status(1)
+    end
+
+    redirect_to merchant_orders_path(params[:order_id])
+  end 
 end
