3b8f0333e9f91b32900fdf08ec16022dc8bcce34
Michael Cooper <macoop2363@gmail.com>
2019-10-29 11:52:02 -0600

    Add functionality to make all of a merchant's items become enabled/disabled when a merchant is enabled/disabled
    Co-authored-by: Laura Schulz <30582885+lrs8810@users.noreply.github.com>
    Co-authored-by: Scott Regenthal <50811220+freeheeling@users.noreply.github.com>
    Co-authored-by: Christopher Kelling <51389815+cjkelling@users.noreply.github.com>

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -10,6 +10,7 @@ class Admin::MerchantsController < Admin::BaseController
     merchant = Merchant.find(params[:merchant_id])
     is_enabled = merchant.enabled?
     merchant.update_attributes(enabled?: !is_enabled)
+    merchant.items.update(active?: !is_enabled)
     if !is_enabled
       flash[:success] = "#{merchant.name} has been enabled!"
     else
