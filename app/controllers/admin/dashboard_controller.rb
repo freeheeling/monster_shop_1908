06a3fd3320df6247189b943e17b38ce24022e7d2
Laura Schulz <lrs8810@gmail.com>
2019-10-28 21:53:01 -0600

    Add update status action to Admin:Dashboard Controller

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -4,4 +4,10 @@ class Admin::DashboardController < Admin::BaseController
   def index
     @orders = Order.sort_orders
   end
+
+  def update_status
+    @order = Order.find(params[:order_id])
+    @order.update_status(2)
+    redirect_to admin_path
+  end
 end
