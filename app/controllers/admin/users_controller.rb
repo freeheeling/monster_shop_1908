ff3c8d47e3b57f038b357ab29868b72fae88e023
Michael Cooper <macoop2363@gmail.com>
2019-10-29 17:17:21 -0600

    Add functionality to Admin::UsersController to toggle a user between enabled and disabled

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -8,4 +8,16 @@ class Admin::UsersController < Admin::BaseController
   def show
     @user = User.find(params[:user_id])
   end
+
+  def toggle_enabled
+    user = User.find(params[:user_id])
+    is_enabled = user.enabled?
+    user.update_attributes(enabled?: !is_enabled)
+    if !is_enabled
+      flash[:notice] = "#{user.name} has been enabled!"
+    else
+      flash[:notice] = "#{user.name} has been disabled!"
+    end
+    redirect_to admin_users_path
+  end
 end
