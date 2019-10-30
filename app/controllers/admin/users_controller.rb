49fafbb4a04c205888e6b5d039f69e3e81a55446
Laura Schulz <lrs8810@gmail.com>
2019-10-30 11:45:05 -0600

    Add edit profile, update profile, and user profile params methods to Admin::Users Controller

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -20,4 +20,24 @@ class Admin::UsersController < Admin::BaseController
     end
     redirect_to admin_users_path
   end
+
+  def edit_profile
+    @user = User.find(params[:user_id])
+  end
+
+  def update_profile
+    @user = User.find(params[:user_id])
+    if @user.update(user_profile_params)
+      flash[:success] = "#{@user.name}'s profile data has been updated!"
+      redirect_to "/admin/users/#{@user.id}"
+    else
+      flash.now[:error] = @user.errors.full_messages.uniq.to_sentence
+      render :edit_profile
+    end
+  end
+
+  private
+    def user_profile_params
+      params.permit(:name, :address, :city, :state, :zip, :email)
+    end
 end
