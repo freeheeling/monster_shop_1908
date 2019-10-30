62e866446b4568418b558ea37a32d2a6d1cb1662
Michael Cooper <macoop2363@gmail.com>
2019-10-30 15:10:56 -0600

    Add test for disabled users to not show up in Merchant#distinct_cities

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -54,7 +54,7 @@ describe Merchant, type: :model do
 
     it 'distinct_cities' do
       chain = @meg.items.create(name: 'Chain', description: "It'll never break!", price: 40, image: 'https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588', inventory: 22)
-      user = User.create(
+      user_1 = User.create(
         name: 'Bob',
         address: '123 Main',
         city: 'Denver',
@@ -63,12 +63,25 @@ describe Merchant, type: :model do
         email: 'bob@email.com',
         password: 'secure'
       )
-      order_1 = user.orders.create!(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17_033)
-      order_2 = user.orders.create!(name: 'Brian', address: '123 Brian Ave', city: 'Denver', state: 'CO', zip: 17_033)
-      order_3 = user.orders.create!(name: 'Dao', address: '123 Mike Ave', city: 'Denver', state: 'CO', zip: 17_033)
+
+      user_2 = User.create(
+        name: 'Dan',
+        address: '123 Main',
+        city: 'Boulder',
+        state: 'CO',
+        zip: 80_303,
+        email: 'Dan@email.com',
+        password: 'secure',
+        enabled?: false
+      )
+      order_1 = user_1.orders.create!(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17_033)
+      order_2 = user_1.orders.create!(name: 'Brian', address: '123 Brian Ave', city: 'Denver', state: 'CO', zip: 17_033)
+      order_3 = user_1.orders.create!(name: 'Dao', address: '123 Mike Ave', city: 'Denver', state: 'CO', zip: 17_033)
+      order_4 = user_2.orders.create!(name: 'Dao', address: '123 Mike Ave', city: 'Boulder', state: 'CO', zip: 80_303)
       order_1.item_orders.create!(item: @tire, price: @tire.price, quantity: 2)
       order_2.item_orders.create!(item: chain, price: chain.price, quantity: 2)
       order_3.item_orders.create!(item: @tire, price: @tire.price, quantity: 2)
+      order_4.item_orders.create!(item: @tire, price: @tire.price, quantity: 2)
 
       expect(@meg.distinct_cities.sort).to eq(%w[Denver Hershey])
     end
